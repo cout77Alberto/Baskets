5b8c4a5a0f66c2db53dd2cdffcfad59aecc70189
Alberto Romañach <albertoromanach@gmail.com>
2018-02-19 15:22:20 -0500

    Navbar initial fragment
    - buttons in wrong place, wrong size, no content yet

--- Baskets/app/src/main/java/edu/osu/baskets/MainActivity.java
+++ Baskets/app/src/main/java/edu/osu/baskets/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.osu.baskets;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Daniel was here
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.navbar_container);
+
+        if (fragment == null){
+            fragment = new NavbarFragment();
+            fm.beginTransaction()
+                    .add(R.id.navbar_container, fragment)
+                    .commit();
+        }
     }
 }
