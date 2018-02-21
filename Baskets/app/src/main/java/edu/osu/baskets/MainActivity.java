99946cb8e9545c8b0d3a08faaff0b10f27e22fde
Sigil46 <lewis.james.sam@gmail.com>
2018-02-21 02:38:42 -0500

    Add log statements to lifecycle methods.

--- Baskets/app/src/main/java/edu/osu/baskets/MainActivity.java
+++ Baskets/app/src/main/java/edu/osu/baskets/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +24,35 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.navbar_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
