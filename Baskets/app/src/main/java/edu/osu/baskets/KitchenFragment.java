98f5ab76353f467669b142d65352ee530c92e572
Daniel <daniel.bond12@yahoo.com>
2018-03-09 22:26:21 -0500

    Recipe list added with recycler view. Buttons added on Kitchen fragment without functionality

--- Baskets/app/src/main/java/edu/osu/baskets/KitchenFragment.java
+++ Baskets/app/src/main/java/edu/osu/baskets/KitchenFragment.java
@@ -3,11 +3,19 @@ package edu.osu.baskets;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+import edu.osu.baskets.recipes.BaseRecipe;
+import edu.osu.baskets.recipes.RecipeBook;
 
 /**
  * Created by Alberto on 2/19/2018.
@@ -16,7 +24,9 @@ import android.widget.Button;
 public class KitchenFragment extends Fragment {
 
     private static final String TAG = "KitchenFragment";
-
+    private Button mRecipesButton, mHistoryButton;
+    private RecyclerView mRecyclerView;
+    private RecipeAdapter mAdapter;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +36,21 @@ public class KitchenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_kitchen, container, false);
+        //mRecipesButton = v.findViewById(R.id.button_recipes);
+        //mHistoryButton = v.findViewById(R.id.button_cooking_history);
+        mRecyclerView = v.findViewById(R.id.recipe_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
         return v;
+    }
+
+    private void updateUI() {
+        RecipeBook crimeLab = RecipeBook.get(getActivity());
+        List<BaseRecipe> crimes = crimeLab.getRecipes();
+
+        mAdapter = new RecipeAdapter(crimes);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -57,5 +81,45 @@ public class KitchenFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy() called");
+    }
+
+    private class RecipeHolder extends RecyclerView.ViewHolder {
+        private TextView mTitleTextView;
+        private BaseRecipe mRecipe;
+
+        public RecipeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_recipe, parent, false));
+
+            mTitleTextView = (TextView) itemView.findViewById(R.id.recipe_title);
+        }
+
+        public void bind(BaseRecipe crime) {
+            mRecipe = crime;
+            mTitleTextView.setText(mRecipe.getTitle());
+        }
+    }
+    private class RecipeAdapter extends RecyclerView.Adapter<RecipeHolder> {
+
+        private List<BaseRecipe> mRecipes;
+
+        public RecipeAdapter(List<BaseRecipe> recipes) {
+            mRecipes = recipes;
+        }
+        @Override
+        public RecipeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            return new RecipeHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mRecipes.size();
+        }
+        @Override
+        public void onBindViewHolder(RecipeHolder holder, int position) {
+            BaseRecipe crime = mRecipes.get(position);
+            holder.bind(crime);
+        }
     }
 }
